23daad5e8e37e49f61d41318c6506a1bf1c8057d
Kuba <kuba.ceg@gmail.com>
2013-05-15 00:13:33 +0200

    Transformation class fixes

--- lib/XsltTransformation.py
+++ lib/XsltTransformation.py
@@ -1,32 +1,14 @@
 __author__ = 'kuba'
 
-import sys, os, getopt, subprocess
+import sys, os, getopt, shlex
+from subprocess import Popen, PIPE, STDOUT
+
 
 
 class XsltTransformation:
     xsltFile = "wsdl2rdf_v3.xsl"
     inputfile = ''
     outputfile = ''
-
-    def getParams(self, argv):
-
-        global inputfile
-        global outputfile
-        try:
-            opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-        except getopt.GetoptError:
-            print 'parser.py -i <directory with wsdl> -o <directory with output>'
-            sys.exit(2)
-        for opt, arg in opts:
-            if opt == '-h':
-                print 'parser.py -i <directory with wsdl> -o <directory with output>'
-                sys.exit()
-            elif opt in ("-i", "--ifile"):
-                self.inputfile = arg
-            elif opt in ("-o", "--ofile"):
-                self.outputfile = arg
-        os.mkdir(self.outputfile)
-
 
     def xsltTransformation(self):
         fileList = os.listdir(self.inputfile)
@@ -35,12 +17,15 @@ class XsltTransformation:
         print "Transforming wsdl\'s..."
         for file in fileList:
             sys.stdout.write('\t' + str(counter) + ' of ' + str(total) + ' files transformed\r')
-            subprocess.Popen(['xsltproc', '--stringparam filename "' + os.path.basename(
-                file) + '" ' + self.xsltFile + " " + file + " > " + self.outputfile + "/" + os.path.basename(file)],
-                             stderr=subprocess.STDOUT, stdout=subprocess.PIPE).communicate()[0]
+            filename = os.path.splitext(os.path.basename(file))[0]
+            out = Popen(shlex.split('xsltproc --stringparam filename "' + filename + '"' + " " + self.xsltFile + " " + self.inputfile+file), stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate()[0]
+            file = open(self.outputfile+'/'+filename+".rdf", 'w')
+            file.write(out)
+            file.close()
             counter += 1
         sys.stdout.write('\t' + str(counter) + ' of ' + str(total) + ' files transformed')
 
-    def __init__(self, argv):
-        self.getParams(argv)
+    def __init__(self, inputfile, outputfile):
+        self.inputfile = inputfile
+        self.outputfile = outputfile
         self.xsltTransformation()
--- parser.py
+++ parser.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import sys, lib
+import sys, lib, os, getopt
+
+
+def getParams(argv):
+    global inputfile
+    global outputfile
+    helpMsg = 'parser.py -i <directory with wsdl> -o <directory with output>'
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print helpMsg
+        sys.exit(2)
+    if len(opts) == 0:
+        print helpMsg
+        sys.exit()
+    for opt, arg in opts:
+        if opt == '-h':
+            print helpMsg
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    if not (os.path.exists(outputfile)):
+        os.mkdir(outputfile)
 
 
 if __name__ == "__main__":
-    xslt = lib.XsltTransformation(sys.argv[1:])
+    getParams(sys.argv[1:])
+    xslt = lib.XsltTransformation(inputfile, outputfile)
